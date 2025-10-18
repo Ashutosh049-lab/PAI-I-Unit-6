9aa83b685bab1b97150259a2c891adef91dd3cc1
Ashutoshkj <jashutosh528@gmail.com>
2025-10-18 14:55:18 +0530

    PAI-01-06

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 
 const express=require("express");
 const connectToDB=require("./config/db");
+const cors=require("cors");
+const dotenv=require("dotenv");
+const loggerMiddleware = require("./middleware/logger.middleware");
+const userRoutes=require("./Routes/user.routes")
+const movieRoutes=require("./Routes/movie.routes")
+const reviewRoutes=require("./Routes/review.routes")
 
 const app=express();
 app.use(express.json());
 
 connectToDB();
+app.use(loggerMiddleware);
+app.use("/user",userRoutes);
+
+app.use("/movies",movieRoutes);
+app.use("/reviews",reviewRoutes);
 
 app.get("/test",(req,res)=>{
     res.send("movie review ");
